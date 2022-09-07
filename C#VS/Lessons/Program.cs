92ade8e6097baa6d34c8c241bc440b6d69a12813
Lubimec_ <dowslf@gmail.com>
2022-09-07 22:02:19 +0500

    k

---
+++ C#VS/Lessons/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lessons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
