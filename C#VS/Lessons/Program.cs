87638ef3a06bf5db0ed177d833c1f222d78404fe
Lubimec_ <dowslf@gmail.com>
2022-09-09 18:58:19 +0500

    k

--- C#VS/Lessons/Program.cs
+++ C#VS/Lessons/Program.cs
@@ -7,6 +7,11 @@ namespace Lessons
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            string str = "5";
+            string str2 = "2";
+
+            Console.WriteLine(str + str2);
         }
     }
 }
