a886d268ed28b4356178f22fade89a666954cf43
Lubimec_ <dowslf@gmail.com>
2022-09-11 14:50:03 +0500

    calculator in sup

--- C#VS/sup/Program.cs
+++ C#VS/sup/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,37 @@ namespace sup
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine("Calculator! Enter the expression!");
+            //string expression = Console.ReadLine();
+
+            //string[] dig = expression.Split(' ');
+
+            //int var1 = Convert.ToInt32(dig[0]);
+            //int var2 = Convert.ToInt32(dig[2]);
+            //string symb1 = dig[1];
+            //int ret;
+            //switch (symb1)
+            //{
+            //    case "+":
+            //        ret = var1 + var2;
+            //        Console.WriteLine($"The result is {ret}");
+            //        break;
+            //    case "-":
+            //        ret = var1 - var2;
+            //        Console.WriteLine($"The result is {ret}");
+            //        break;
+            //    case "*":
+            //        ret = var1 * var2;
+            //        Console.WriteLine($"The result is {ret}");
+            //        break;
+            //    case "/":
+            //        ret = var1 / var2;
+            //        Console.WriteLine($"The result is {ret}");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Wrong expression!");
+            //        break;
+            }
         }
     }
 }
