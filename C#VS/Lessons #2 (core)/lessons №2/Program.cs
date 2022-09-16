6392b2a7d0db77ecf583dfcd77ad8719c58c9f53
Lubimec_ <dowslf@gmail.com>
2022-09-16 19:13:10 +0500

    k

--- C#VS/Lessons #2 (core)/lessons №2/Program.cs
+++ C#VS/Lessons #2 (core)/lessons №2/Program.cs
@@ -8,11 +8,25 @@ namespace Lessons_core
         static void Main(string[] argc)
         {
             //// Индексы и диапазоны в C#                                                   Индексы и диапазоны в C#
-            int[] anArray = { 2, 44, 51, 22 };
+            //int[] anArray = { 2, 44, 51, 22 };
 
-            Console.WriteLine(anArray[^1]);
-            int[] beb = anArray[1..4];
-            int[] aa = anArray[..3]; //с самого начала и до3
+            //Console.WriteLine(anArray[^1]);
+            //int[] beb = anArray[1..4];
+            //int[] aa = anArray[..3]; //с самого начала и до 3
+
+            //Index anIndex = ^2;
+            //Console.WriteLine(anArray[anIndex]);
+
+            //Console.WriteLine($"value {anIndex.Value} isFromEnd {anIndex.IsFromEnd}");
+            
+            //Range aRange = new Range();
+            //int[] anArray2 = anArray[aRange];
+            //Range range = ^4..^1;
+            //Console.WriteLine();
+            //Console.WriteLine();
+
+            string str = "Hello World!";
+            Console.WriteLine(str[1]);
         }
     }
 }
