974b4d4ceca1b7d718d434c426b8d5912f685cea
Lubimec_ <dowslf@gmail.com>
2022-10-22 22:13:14 +0500

    КОНСТРУКТОР КЛАССА

---
+++ C#VS/OOP/Gun.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Gun
+    {
+        /* СПЕЦИАЛЬНЫЙ СНИППЕТ (написать ctor и давжды на tap) */
+        public Gun(bool isLoaded) // Конструктор такой же компонент класса, как и методы!
+        {
+            this.isLoaded = isLoaded; //this полезная темая для помощи компилятора определить переменные с одинаковыми названиями
+        }
+
+
+
+        private bool isLoaded = false;
+        private void Reload()
+        {
+            Console.WriteLine("Reloading...");
+            isLoaded = true;
+            Console.WriteLine("Reloaded!");
+        }
+
+        public void Shoot()
+        {
+            if (!isLoaded)
+            {
+                Console.WriteLine("The gun is empty!");
+                Reload();
+                Shoot();
+            }
+            else
+            {
+                Console.WriteLine("Fire!");
+                isLoaded = false;
+            }    
+        }
+    }
+}
