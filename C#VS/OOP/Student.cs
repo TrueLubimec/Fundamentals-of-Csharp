02be13dc0c7b1b58ca3367ee406fac5da9d79801
Lubimec_ <dowslf@gmail.com>
2022-10-23 01:04:04 +0500

    Ключевое слово this.

--- C#VS/OOP/Student.cs
+++ C#VS/OOP/Student.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace OOP
 {
     internal class Student
     {
         public Guid id;
         public string? name;
-        public int age;
+        public DateTime age;
         public int group;
         
         //// МЕТОДЫ И КЛАССЫ | ВЫЗОВ МЕТОДА ОБЪЕКТА КЛАССА                          МЕТОДЫ И КЛАССЫ | ВЫЗОВ МЕТОДА ОБЪЕКТА КЛАССА
@@ -26,8 +27,21 @@ namespace OOP
         {
             return $"{name} {age}";
         }
+
+        //// Ключевое слово this.                                                   Ключевое слово this.
+        public Student(string name, DateTime age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        // Используется, когда есть неоднозначность в названии переменных
+        public Student(string name, DateTime age, int group) : this(name, age) // То есть он делегирует эти данные другому конструктору
+        {
+            this.id = new Guid();
+            this.group = group;
+        }
     }
-    
+
     class Point
     {
         private int x;
