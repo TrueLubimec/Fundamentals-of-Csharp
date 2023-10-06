2a67cd0c70a97159736fc0833e3ffa547911ff6d
Lubimec_ <dowslf@gmail.com>
2023-10-06 13:53:36 +0500

    obj, IEnum, date

--- UnitTesting/NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
+++ UnitTesting/NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
@@ -1,14 +1,23 @@
-﻿namespace NetworkUtility.Tests;
+﻿using FluentAssertions.Extensions;
+using System.Net.NetworkInformation;
+
+namespace NetworkUtility.Tests;
 public class NetworkServiceTests
 {
+    private readonly NetworkService networkService;
+
+    public NetworkServiceTests()
+    {
+        networkService = new NetworkService();
+    }
     [Fact]
     public void NetworkService_SendPing_ReturnString()
     {
         //Arrange
-        var pingService = new NetworkService();
+        
 
         //Act
-        var result = pingService.SendPing();
+        var result = networkService.SendPing();
 
         //Assert
         result.Should().NotBeNullOrWhiteSpace();
@@ -25,15 +34,68 @@ public class NetworkServiceTests
     public void Network_PingTimeout_ReturnInt(int a, int b, int expected)
     {
         //Arrange
-        var pingService = new NetworkService();
+
 
         //Act
-        var result = pingService.PingTimeout(a, b);
+        var result = networkService.PingTimeout(a, b);
 
         //Assert
         result.Should().Be(expected);
         result.Should().BeGreaterThanOrEqualTo(2);
         result.Should().NotBeInRange(-1, 0);
+
+    }
+
+    [Fact]
+    public void NetworkService_LastPingDate_ReturnDate()
+    {
+        //Arrange
+
+
+        //Act
+        var result = networkService.LastPingDate();
+
+        //Assert
+        result.Should().BeAfter(1.January(2010));
+        result.Should().BeBefore(1.January(2030));
+    }
+
+    [Fact]
+    public void NetworkService_GetPingOptions_ReturnsObject()
+    {
+        //Arrange
+        var expected = new PingOptions()
+        {
+            DontFragment = true,
+            Ttl = 1
+        };
+
+        //Act
+        var result = networkService.GetPingOptions();
+
+        //Assert
+        result.Should().BeOfType<PingOptions>();
+        result.Should().BeEquivalentTo(expected);
+        result.Ttl.Should().Be(1);
+    }
+
+    [Fact]
+    public void NetworkService_MostRecentPings_ReturnObjects()
+    {
+        //Arrange
+        var expected = new PingOptions()
+        {
+            DontFragment = true,
+            Ttl = 1
+        };
+
+        //Act
+        var result = networkService.MostRecentPings();
+
+        //Assert
+        result.Should().BeAssignableTo<IEnumerable<PingOptions>>();
+        result.Should().ContainEquivalentOf(expected);
+        result.Should().Contain(x => x.DontFragment == true);
 
     }
 }
--- UnitTesting/NetworkUtility/Ping/NetworkService.cs
+++ UnitTesting/NetworkUtility/Ping/NetworkService.cs
@@ -1,4 +1,6 @@
-﻿namespace NetworkUtility;
+﻿using System.Net.NetworkInformation;
+
+namespace NetworkUtility;
 public class NetworkService
 {
     public string SendPing()
@@ -11,5 +13,42 @@ public class NetworkService
     public int PingTimeout(int a, int b)
     {
         return a * b;
+    }
+
+    public DateTime LastPingDate()
+    {
+        return DateTime.Now;
+    }
+
+    public PingOptions GetPingOptions()
+    {
+        return new PingOptions()
+        {
+            DontFragment = true,
+            Ttl = 1
+        };
+    }
+
+    public IEnumerable<PingOptions> MostRecentPings()
+    {
+        IEnumerable<PingOptions> pingOptions = new[]
+        {
+            new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            },
+            new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            },
+            new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            }
+        };
+        return pingOptions;
     }
 }
