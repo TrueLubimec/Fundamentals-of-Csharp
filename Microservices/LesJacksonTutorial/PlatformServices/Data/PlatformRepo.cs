2d4e30127127dbfe6d4ef789c1e1d4a9cf965494
Lubimec_ <dowslf@gmail.com>
2023-11-12 20:00:28 +0500

    lil prep

---
+++ Microservices/LesJacksonTutorial/PlatformServices/Data/PlatformRepo.cs
@@ -0,0 +1,33 @@
+﻿using PlatformService.Data;
+using PlatformService.Models;
+
+namespace PlatformService.Data;
+public class PlatformRepo : IPlatformRepo
+{
+    private readonly AppDbContext dbContext;
+
+    public PlatformRepo(AppDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    public void CreatePlatform(Platform platform)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Platform GetPlatform(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Platform> GetPlatforms()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool SaveChanges()
+    {
+        throw new NotImplementedException();
+    }
+}
--- Microservices/LesJacksonTutorial/PlatformServices/Program.cs
+++ Microservices/LesJacksonTutorial/PlatformServices/Program.cs
@@ -1,12 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<AppDbContext>(options =>
+                                            options.UseInMemoryDatabase("InMemo"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
